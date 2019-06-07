a08a142ae9e10e1843201dd5d98d62d2fd22259b
Eric Young <youngeric@gmail.com>
2019-06-07 17:03:02 -0500

    .

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// make all paths relative to the application folder
+chdir(dirname(__DIR__));
+
+require 'application/controllers/IndexController.php';
+require 'application/controllers/CalculatorController.php';
+
+if (!isset($_GET['controller'])) {
+  $controllerName = 'controllers\\IndexController';
+  $action = 'indexAction';
+} else {
+  $controllerName = 'controllers\\' . ucfirst($_GET['controller']) . 'Controller';
+  $action = $_GET['action'] . 'Action';
+}
+
+$controller = new $controllerName();
+$controller->$action();
